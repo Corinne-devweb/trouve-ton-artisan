4b6a2e46dbc6e395ffea22e2225a33f3e8db816d
CocoLaDev <corinne.devweb1@gmail.com>
2025-08-31 13:10:05 +0200

    Correction erreurs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+
+// Import des composants
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+// Import des pages
+import Home from "./pages/Home/Home";
+import ArtisansList from "./pages/ArtisansList/ArtisansList";
+import ArtisanDetail from "./pages/ArtisanDetail/ArtisanDetail";
+import LegalPage from "./pages/LegalPages/LegalPage";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            {/* Page d'accueil */}
+            <Route path="/" element={<Home />} />
+
+            {/* Pages des catégories d'artisans */}
+            <Route
+              path="/batiment"
+              element={<ArtisansList category="batiment" />}
+            />
+            <Route
+              path="/services"
+              element={<ArtisansList category="services" />}
+            />
+            <Route
+              path="/fabrication"
+              element={<ArtisansList category="fabrication" />}
+            />
+            <Route
+              path="/alimentation"
+              element={<ArtisansList category="alimentation" />}
+            />
+
+            {/* Résultats de recherche */}
+            <Route path="/search/:query" element={<ArtisansList />} />
+
+            {/* Détail d'un artisan */}
+            <Route path="/artisan/:id" element={<ArtisanDetail />} />
+
+            {/* Pages légales */}
+            <Route
+              path="/mentions-legales"
+              element={<LegalPage type="mentions" />}
+            />
+            <Route
+              path="/donnees-personnelles"
+              element={<LegalPage type="privacy" />}
+            />
+            <Route
+              path="/accessibilite"
+              element={<LegalPage type="accessibility" />}
+            />
+            <Route path="/cookies" element={<LegalPage type="cookies" />} />
+
+            {/* Page 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
